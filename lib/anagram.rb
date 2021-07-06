4d2964ecb5433da229900b2719a130b714299505
alexanderhulteng <alexanderhulteng@gmail.com>
2021-07-06 12:29:48 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+
+
+
+
+class Anagram
+
+    attr_accessor :detector
+    
+    def initialize(phrase)
+        @detector = phrase
+    end
+
+    def match(data)
+       
+        data.select do 
+            |x| x.split("").sort == @detector.split("").sort
+        end
+           
+        
+    end
+
+
+
+
+end
